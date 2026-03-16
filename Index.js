b075e605aad411ee15bed75f663ad198289cd00b
marcus luc <marcuslucien.almeida@gmail.com>
2026-03-16 12:31:27 -0300

    minha atualização de desafio

--- Index.js
+++ Index.js
@@ -1,1 +1,18 @@
-git 
+let nomeHeroi = "Holmes"
+let xp = 5.650
+
+if (xp >= 2.000 && xp <= 3.000) {
+    console.log( nomeHeroi + " é um herói nivel bronze")
+
+}else if (xp >= 3.001 && xp <= 4.000) {
+    console.log( nomeHeroi + " é um herói nivel prata")
+    
+}else if (xp >= 4.001 && xp <= 5.000) {
+    console.log( nomeHeroi + " é um herói nivel ouro")
+    
+}else if (xp >= 5.001 && xp <= 6.000) {
+    console.log( nomeHeroi + " é um herói nivel diamante")
+    
+}else {
+    console.log( nomeHeroi + " é um herói nivel mestre")   
+}
